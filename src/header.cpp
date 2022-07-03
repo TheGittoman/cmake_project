63b7320c9498d2f1d37e9b567af9b9fa1ffcbca0
Gittoman <anonyme.and.free@protonmail.com>
2022-07-03 14:36:39 +0300

    initial commit

---
+++ src/header.cpp
@@ -0,0 +1,8 @@
+#include "header.h"
+
+#include <iostream>
+
+void test_func()
+{
+    std::cout << "This is from header.h\n";
+}
---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+
+#include "header.h"
+
+int main(int argc, char** argv){
+    std::cout << "Hello World!\n";
+    test_func();
+    return 0;
+}
